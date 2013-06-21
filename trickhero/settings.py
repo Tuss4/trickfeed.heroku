1052675b8dbb1793b402d0c38c80fa1697d34d27
Tuss4 <tuss4dbfn@gmail.com>
2013-06-21 15:16:18 -0500

    starting to get a little frustrated with these settings...

--- trickhero/settings.py
+++ trickhero/settings.py
@@ -5,7 +5,7 @@ DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-     ('TJ S', 'tuss4dbfn@gmail.com'),
+     #('TJ S', 'tuss4dbfn@gmail.com'),
 )
 
 MANAGERS = ADMINS
@@ -24,7 +24,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['www.herkouapp.com']
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
