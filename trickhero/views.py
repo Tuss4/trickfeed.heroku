6078520336f8ede4ab551e994bec8d0b595591e1
Tuss4 <tuss4dbfn@gmail.com>
2013-06-21 13:15:28 -0500

    Added a little bit more thought to the front end

--- trickhero/views.py
+++ trickhero/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
+from django.template.loader import get_template
+from django.template import Template, Context
 import gdata.youtube
 import gdata.youtube.service
 
 def trick(request):
-	a = ["<ul>"]
+	a = []
 	def GetAndPrintVideoFeed(uri):
 		yt_service = gdata.youtube.service.YouTubeService()
 		feed = yt_service.GetYouTubeVideoFeed(uri)
@@ -11,8 +13,9 @@ def trick(request):
 			for thumbnail in entry.media.thumbnail:
 				t = []
 				t.append(thumbnail.url)
-			a.append("<li><a href='"+entry.media.player.url+"'>"+entry.media.title.text+"</a><br /><img src="+t[0]+" alt="+entry.media.title.text+" title="+entry.media.title.text+" /></li>")
+			a.append("<a href='"+entry.media.player.url+"'><img src="+t[0]+" alt="+entry.media.title.text+" title="+entry.media.title.text+" /></a>")
 	datafeed = "http://gdata.youtube.com/feeds/api/videos?q=tricking&orderby=published&category=tricking&max-results=30"
 	GetAndPrintVideoFeed(datafeed)
-	a.append("</ul>")
-	return HttpResponse(a)
+	t = get_template('base.html')
+	html = t.render(Context({'content': a}))
+	return HttpResponse(html)
