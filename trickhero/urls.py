9e008ce48c3a514f2e73c86275f4fe91e8d10ee3
Tuss4 <tuss4dbfn@gmail.com>
2013-07-01 01:39:13 -0500

    Ahhh stupid mistakes are stupid.

--- trickhero/urls.py
+++ trickhero/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
 from trickhero.views import trick
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^$', trick),
@@ -14,5 +14,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^homebase/$', include(admin.site.urls)),
 )
