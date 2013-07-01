a36452a33d7584f7b87bd57cb74a17e593c2729d
Tuss4 <tuss4dbfn@gmail.com>
2013-07-01 09:25:08 -0500

    registration form password input update

--- trickhero/forms.py
+++ trickhero/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 class New_user(forms.Form):
 	username = forms.CharField()
-	password = forms.CharField()
+	password = forms.CharField(widget=forms.PasswordInput)
 	email = forms.EmailField()
