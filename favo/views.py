5f1035dfaab843a68116bca8cc658ee3f4c7eefa
Tuss4 <tuss4dbfn@gmail.com>
2013-07-01 02:16:08 -0500

    adding ze files

---
+++ favo/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import *
+from django.http import *
+from django.core.context_processors import csrf
+from favo.models import Favorite
+
+def success(request): 
+	#How a user adds videos to their favorites. 
+	#If the video is already saved it won't add. 
+	if request.method == 'POST':
+		f = Favorite(user = request.user.username,
+			video = request.POST['video'])
+		favorites = Favorite.objects.all()
+		v = request.POST['video']
+		fs = Favorite.objects.filter(video=v)
+		fu = Favorite.objects.filter(user=request.user.username)
+		if fs and fu:
+			return HttpResponse(v + " is already in your favorites.")
+		f.save()
+		return HttpResponse("Video: "+ v + " was added to your favorites!")
+
+def list_fav(request):
+	#The "my favorites" page
+	if request.user.is_authenticated():
+		videos = Favorite.objects.filter(user=request.user.username)
+		return render(request, "favorites.html", {"videos": videos})
+
--- trickhero/views.py
+++ trickhero/views.py
@@ -1,11 +1,15 @@
-from django.http import HttpResponse
+from django.http import *
 from django.template.loader import get_template
 from django.template import Template, Context
+from django.shortcuts import *
 import gdata.youtube
 import gdata.youtube.service
+from favo.forms import AddFav
 
 def trick(request):
 	a = []
+	tf_user = request.user.is_authenticated()
+	form = AddFav(request.POST)
 	def GetAndPrintVideoFeed(uri):
 		yt_service = gdata.youtube.service.YouTubeService()
 		feed = yt_service.GetYouTubeVideoFeed(uri)
@@ -13,9 +17,7 @@ def trick(request):
 			for thumbnail in entry.media.thumbnail:
 				t = []
 				t.append(thumbnail.url)
-			a.append("<a onclick=changeThisId('"+entry.media.player.url[32:43]+"')><img src='"+t[0]+"' alt='"+entry.media.title.text+"' title='"+entry.media.title.text+"' /></a>")
+			a.append("<a onclick=changeThisId('"+entry.media.player.url[32:43]+"')><img src="+t[0]+" alt='"+entry.media.title.text+"' title='"+entry.media.title.text+"' /></a>")
 	datafeed = "http://gdata.youtube.com/feeds/api/videos?q=tricking&orderby=published&category=tricking&max-results=30"
 	GetAndPrintVideoFeed(datafeed)
-	t = get_template('base.html')
-	html = t.render(Context({'content': a}))
-	return HttpResponse(html)
+	return render(request, 'feed.html', {'content': a, 'form': form, 'user': tf_user})
