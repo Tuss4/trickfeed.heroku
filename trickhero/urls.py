6850b0ec31da2e528e4e93c86b61b48b9ee2c98e
Tuss4 <tuss4dbfn@gmail.com>
2013-07-02 11:29:43 -0500

    new settings

--- trickhero/urls.py
+++ trickhero/urls.py
@@ -24,3 +24,8 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^homebase/', include(admin.site.urls)),
 )
+urlpatterns += patterns('',
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT, 
+        }),
+    )
