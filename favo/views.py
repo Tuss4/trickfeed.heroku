daa5de71b97cd959fa99190651a5a0e1dd641e04
Tuss4 <tuss4dbfn@gmail.com>
2013-07-04 10:04:18 -0500

    another test

--- favo/views.py
+++ favo/views.py
@@ -33,6 +33,6 @@ def del_fav(request):
 	if request.method == 'POST':
 		for i in request.POST.get("video"):
 			if i:
-				return HttpResponseRedirect("/myfavs/")
+				return HttpResponse(request.POST.get("value"))
 
 
