369a87bd8e26f0294c5cbe084d0337c33562bd0e
Tuss4 <tuss4dbfn@gmail.com>
2013-07-06 01:16:52 -0500

    Base.html: testing $.post() method. with #addToFav submit event.

--- trickhero/settings.py
+++ trickhero/settings.py
@@ -119,8 +119,6 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     'favo',
-    'dajaxice',
-    'dajax',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
