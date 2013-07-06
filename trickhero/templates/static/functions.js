09d163ccd22675de41046629504bf1481daf1c5f
Tuss4 <tuss4dbfn@gmail.com>
2013-07-06 01:51:11 -0500

    Moving functions to a separate functions.js static file.

---
+++ trickhero/templates/static/functions.js
@@ -0,0 +1,27 @@
+function getThisId(datum){
+	var newUrl = "http://youtube.com/embed/"+datum+"?autoplay=1&allowFullScreen=true";
+	return newUrl;
+}
+function changeThisId(datum){
+	document.getElementById("ytplayer").setAttribute("src",getThisId(datum));
+	document.getElementById("id_video").setAttribute("value",datum);
+}
+$(document).ready(function(){
+	$(".tvid").click(function(){
+		$("#overlay").show();
+	})
+	$("#close").click(function(){
+		$("#overlay").hide();
+		$("#ytplayer").attr("src","http://www.youtube.com/embed/");
+		$("#addToFav").removeClass("hidden");
+	})
+	$("#addToFav").submit(function(){
+		$("#addToFav").addClass("hidden");
+		$.post('/success/', $(this).serialize(), function(data){
+			$.done(function(){
+				$("#message").html("Added to favorites!");
+			});
+		});
+		return false;
+	})
+})
